f11d2b1bd5dfb1d65daa50b5ac2283a752ebc30c
Vladimir Albrekht <vladimir_albreht@mail.ru>
2023-05-08 00:39:00 +0800

    S14:5

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const { celebrate, Joi } = require('celebrate');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
-const handleErrors = require('./middlewares/errorHandler');
+const errorHandler = require('./middlewares/errorHandler');
 
 const app = express();
 app.use(cookieParser());
@@ -19,20 +20,39 @@ app.use((req, res, next) => {
 });
 
 // Подключаемся к серверу MongoDB
-mongoose.connect('mongodb://localhost:27017/mestobd', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect('mongodb://localhost:27017/mestobd', { useNewUrlParser: true, useUnifiedTopology: true });
 
 // Подключаем маршруты
 app.use('/users', usersRouter);
 app.use('/cards', cardsRouter);
 
+// Middleware для проверки тела запроса с помощью celebrate
+app.use(
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    }),
+  }),
+);
+
+// Middleware для обработки ошибок валидации
+app.use((err, req, res, next) => {
+  if (err.joi) {
+    // Если ошибка валидации, отправляем ответ с кодом статуса 400 и сообщением об ошибке
+    res.status(400).json({ message: err.joi.details[0].message });
+  } else {
+    // Если другая ошибка, передаем управление следующему middleware
+    next(err);
+  }
+});
+
 // Middleware для обработки ошибок celebrate
 app.use(errors());
 
 // Middleware для обработки ошибок
-app.use(handleErrors);
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log('Server started on port 3000');

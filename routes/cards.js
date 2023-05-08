94244787d8ecd3b28f840bc77841bab675c68526
Vladimir Albrekht <vladimir_albreht@mail.ru>
2023-05-08 17:30:56 +0800

    Sprint 14:8

--- routes/cards.js
+++ routes/cards.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const {
   getCards,
   createCard,
@@ -7,19 +8,21 @@ const {
   dislikeCard,
 } = require('../controllers/cards');
 
+const createCardSchema = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30).required(),
+    link: Joi.string().required().pattern(/^(https?:\/\/)(www\.)?[\w-]+(\.[\w-]+)+([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?#?$/),
+  }),
+});
+
 // возвращает все карточки
 router.get('/', getCards);
-
 // создает карточку
-router.post('/', createCard);
-
+router.post('/', createCardSchema, createCard);
 // удаляет карточку по идентификатору
 router.delete('/:cardId', deleteCard);
-
 // ставит лайк карточке
 router.put('/:cardId/likes', likeCard);
-
 // убирает лайк с карточки
 router.delete('/:cardId/likes', dislikeCard);
-
 module.exports = router;
